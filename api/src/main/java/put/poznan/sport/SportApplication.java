78ee5705b2e7ae9b3e6058ce3459510550c33473
Pozeraczoinator <134403595+Pozeraczoinator@users.noreply.github.com>
2025-04-02 21:12:51 +0200

    Update SportApplication.java

--- api/src/main/java/put/poznan/sport/SportApplication.java
+++ api/src/main/java/put/poznan/sport/SportApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class  SportApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		//Dotenv dotenv = Dotenv.load();
 
 		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
