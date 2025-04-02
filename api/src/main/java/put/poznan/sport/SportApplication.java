627b909c8dac4e924d610c23e694098516ca4478
Pozeraczoinator <134403595+Pozeraczoinator@users.noreply.github.com>
2025-04-02 21:20:08 +0200

    Update SportApplication.java without env

--- api/src/main/java/put/poznan/sport/SportApplication.java
+++ api/src/main/java/put/poznan/sport/SportApplication.java
@@ -1,20 +1,25 @@
 package put.poznan.sport;
 
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class  SportApplication {
 
-	public static void main(String[] args) {
-		//Dotenv dotenv = Dotenv.load();
+    public static void main(String[] args) {
+        // Pobieranie zmiennych środowiskowych za pomocą System.getenv()
+        String dbUrl = System.getenv("DB_URL");
+        String dbUsername = System.getenv("DB_USERNAME");
+        String dbPassword = System.getenv("DB_PASSWORD");
 
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        // Ustawienie zmiennych jako właściwości systemowe (opcjonalne, jeśli potrzebujesz)
+        System.setProperty("DB_URL", dbUrl);
+        System.setProperty("DB_USERNAME", dbUsername);
+        System.setProperty("DB_PASSWORD", dbPassword);
 
-		SpringApplication.run(SportApplication.class, args);
+        // Uruchomienie aplikacji
+        SpringApplication.run(SportApplication.class, args);
 	}
 }
 
